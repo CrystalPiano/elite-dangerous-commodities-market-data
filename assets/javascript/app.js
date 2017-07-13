609055d5badb6edcf52ce7ef41b26812b8a60d3d
LucFII <LSFIIChi@Gmail.com>
2017-07-13 18:58:26 -0500

    updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,11 @@
-https://eddb.io/archive/v5/commodities.json
+//https://eddb.io/archive/v5/commodities.json
 
+
+jQuery.ajaxPrefilter(function(options) {
+    if (options.crossDomain && jQuery.support.cors) {
+        options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+    }
+});
 
   // Here we are building the URL we need to query the elite dangerous commodities database
   var queryURL = "https://eddb.io/archive/v5/commodities.json";
@@ -9,13 +15,15 @@ https://eddb.io/archive/v5/commodities.json
         url: queryURL,
         method: "GET"
       }).done(function(response) {
-      	console.log(response)
+      	console.log(response[46-1])
 
       // Log the queryURL
         console.log(queryURL);
 
         // Transfer content to HTML
-        $("#result").html(response.response.id[2].name);
+        $("#name").append(response[46-1].name);
+        $("#price").append(response[46-1].average_price);
+        $("#category").append(response[46-1].category.name);
         // Log the data in the console as well
         //console.log("Wind Speed: " + response.wind.speed);
       });
